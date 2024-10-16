30c5d2ba579761c72cfa4f16882d841dc87bb552
Ariadna Valencia <ariadna.valencia1999@gmail.com>
2024-10-15 21:16:21 -0300

    correccion de errores

--- blog_app/accounts/views.py
+++ blog_app/accounts/views.py
@@ -6,13 +6,13 @@ from .forms import SignupForm
 
 def signup(request):
     if request.method == 'POST':
-        form = SignupForm()(request.POST)
+        form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect('profile')
+            return redirect('about')
     else:
-        form = SignupForm()()
+        form = SignupForm()
     return render(request, 'accounts/signup.html', {'form': form})
 
 def profile_view(request):
--- blog_app/blog_app/urls.py
+++ blog_app/blog_app/urls.py
@@ -6,7 +6,6 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('about/', views.about, name='about'),
-    path('pages/', include('pages.urls')),
     path('accounts/', include('accounts.urls')),
     path('chat/', include('chat.urls')),
 ]
